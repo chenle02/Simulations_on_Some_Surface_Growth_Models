539d30a5439d10b3ff75392bea15957ae484c42b
chenle02 <chenle02@gmail.com>
2024-01-30 12:52:05 -0600

    fix(test_I.py): change print statements to refer to I piece instead of O piece fix(test_I_output.txt): change print statements to refer to I piece instead of O piece fix(test_J.py): change print statements to refer to J piece instead of O piece
    fix(test_J_output.txt): change piece name from O to J in test output
    fix(test_L.py): change piece name from O to L in test output
    
    fix(test_L_output.txt): change piece name from O to L in test output
    fix(test_S.py): change piece name from O to S in test output
    
    fix(test_S_output.txt): change 'O piece' to 'S piece' in test output
    fix(test_T.py): change 'O piece' to 'T piece' in test output
    
    fix(test_T_output.txt): change piece name from 'O' to 'T' in test output
    fix(test_Z.py): change piece name from 'O' to 'Z' in test output
    
    fix(test_Z_output.txt): change piece name from O to Z and update test rotation number

--- tests/indivisual-pieces/test_J.py
+++ tests/indivisual-pieces/test_J.py
@@ -17,7 +17,7 @@ def test_J():
 
         print("First, the sticky case:")
         for rot in range(4):
-            print("O piece, Test rotation ", rot)
+            print("J piece, Test rotation ", rot)
             TB.reset()
             i = 0
             while i < TB.steps:
@@ -30,7 +30,7 @@ def test_J():
 
         print("Second, the non-sticky case:")
         for rot in range(4):
-            print("O piece, Test rotation ", rot)
+            print("J piece, Test rotation ", rot)
             TB.reset()
             i = 0
             while i < TB.steps:
--- tests/indivisual-pieces/test_L.py
+++ tests/indivisual-pieces/test_L.py
@@ -17,7 +17,7 @@ def test_L():
 
         print("First, the sticky case:")
         for rot in range(4):
-            print("O piece, Test rotation ", rot)
+            print("L piece, Test rotation ", rot)
             TB.reset()
             i = 0
             while i < TB.steps:
@@ -32,7 +32,7 @@ def test_L():
 
         print("Second, the non-sticky case:")
         for rot in range(4):
-            print("O piece, Test rotation ", rot)
+            print("L piece, Test rotation ", rot)
             TB.reset()
             i = 0
             while i < TB.steps:
