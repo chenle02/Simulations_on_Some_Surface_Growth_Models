47abdc84433a2040103bf04f41dce49186f5fda5
chenle02 <chenle02@gmail.com>
2024-03-04 13:48:14 -0500

    Exp10 minor update

--- Experiments/exp10/checkstatus.sh
+++ Experiments/exp10/checkstatus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define an array of file patterns
-declare -a patterns=("*w=50_*.joblib" "*w=100_*.joblib" "*w=150_*.joblib")
+declare -a patterns=("*w=50_*.joblib" "*w=80_*.joblib" "*w=100_*.joblib" "*w=150_*.joblib")
 
 # Header of the table
 printf "%-40s %-20s\n" "Pattern" "Numbers of files"
