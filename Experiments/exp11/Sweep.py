bf10934cd883ef99670eacdb16286d5903a610b4
chenle02 <chenle02@gmail.com>
2024-03-06 08:57:51 -0500

    refactor(Sweep.py): remove commented out code and update file paths feat(checkstatus.sh): add script to check the number of files matching patterns

--- Experiments/exp11/Sweep.py
+++ Experiments/exp11/Sweep.py
@@ -3,11 +3,6 @@ from tetris_ballistic.sweep_parameters import sweep_parameters as sp
 from tetris_ballistic.data_analysis_utilities import retrieve_fluctuations, insert_joblibs
 from tetris_ballistic.tetris_ballistic import Tetris_Ballistic
 
-# pattern = "../../Experiments/exp10/*w=50*.joblib"
-# pattern = "../../Experiments/exp10/*w=80*.joblib"
-pattern = "../../Experiments/exp10/*.joblib"
-insert_joblibs(pattern, verbose=True)
-
 ListWidth = [50, 80, 100, 150]
 ListRandomSeeds = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
 # config_patterns = ["*piece_19_sticky.yaml",
