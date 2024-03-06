ddbe733ad498a6053f7b1422a6acfcd7cc4ec8ad
chenle02 <chenle02@gmail.com>
2024-03-06 01:44:14 -0600

    Working on exp11.

---
+++ Experiments/exp11/Sweep.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from tetris_ballistic.sweep_parameters import sweep_parameters as sp
+
+
+ListWidth = [50, 80, 100, 150]
+ListRandomSeeds = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+# config_patterns = ["*piece_19_sticky.yaml",
+#                    "*piece_19_nonsticky.yaml",
+#                    "*piece_0*.yaml",
+#                    "*type_1*.yaml",
+#                    "*type_2*.yaml",
+#                    "*type_3*.yaml",
+#                    "*type_4*.yaml",
+#                    "*type_5*.yaml",
+#                    "*type_6*.yaml"]
+config_patterns = ["*.yaml"]
+sp(list_width=ListWidth,
+   list_random_seeds=ListRandomSeeds,
+   config_patterns=config_patterns)
---
+++ Experiments/exp11/readme.md
@@ -0,0 +1,4 @@
+The aim of this experiment is to determine the range to compute the slop. 
+1. One needs to go beyond the initial Guassian phase.
+2. One also needs to stop before the saturation.
+3. We want to identify this while taking into account of the scaling, so all widths will come together.
