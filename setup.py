d63aa053ffcbb35ff68efb8e26f1aff41d2d023f
chenle02 <chenle02@gmail.com>
2024-03-07 01:39:21 -0500

    chore(setup.py): bump version to 1.2.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='tetris_ballistic',
-    version='1.2.5',
+    version='1.2.6',
     packages=find_packages(),
     # entry_points={
     #     'console_scripts': [
