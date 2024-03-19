b3bffc539ab365de8cd16394b36a3152dad9e850
chenle02 <chenle02@gmail.com>
2024-03-18 22:32:28 -0500

    Run on Easley

--- Experiments/exp13/Sweep.py
+++ Experiments/exp13/Sweep.py
@@ -2,7 +2,7 @@
 from tetris_ballistic.sweep_parameters import sweep_parameters as sp
 from tetris_ballistic.data_analysis_utilities import insert_joblibs
 
-ListWidth = [50, 80, 100, 150]
+ListWidth = [50, 80, 100, 150, 200]
 ListRandomSeeds = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
 config_patterns = ["*.yaml"]
 sp(list_width=ListWidth,
