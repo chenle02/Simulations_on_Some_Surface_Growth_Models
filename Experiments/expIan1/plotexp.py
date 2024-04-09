09c94473db71b2658a276d932f1a697b4912aa8f
ianruau <iir0001@auburn.edu>
2024-04-09 12:05:27 -0500

    Must add new compute slope to plotexp.py

--- Experiments/expIan1/plotexp.py
+++ Experiments/expIan1/plotexp.py
@@ -12,8 +12,7 @@ import os
 # import re
 from tetris_ballistic.tetris_ballistic import Tetris_Ballistic
 import matplotlib.pyplot as plt
-from shapely.geometry import LineString, MultiLineString
-from intersect import intersection
+from shapely.geometry import LineString
 
 # Regex pattern to match filename and extract components
 # pattern_parse = r'config_(?P<type>piece_\d+)_combined_(?P<percentage>percentage_\d+)_w=(?P<width>\d+)_seed=(?P<seed>\d+).joblib'
@@ -48,8 +47,8 @@ alpha = 0.1
 beta = 0.9
 logfl = np.log10(fl)
 maxfl = np.max(logfl)
-alphaline = np.full((fl.size), alpha * maxfl) #  Horizontal line at alpha level
-betaline = np.full((fl.size), beta * maxfl) # Horizontal line at beta level
+alphaline = np.full((fl.size), alpha * maxfl)  # Horizontal line at alpha level
+betaline = np.full((fl.size), beta * maxfl)  # Horizontal line at beta level
 print('Maximum is ', maxfl)
 print('Fluctuation vector size= ', fl.size)
 slope1 = np.polyfit(np.log10(range(1, fl.size)), np.log10(fl[1:fl.size]), 1)
@@ -69,23 +68,33 @@ logflgraph = LineString(np.column_stack((np.log10(range(fl.size)), logfl)))
 intersection_af = alphagraph.intersection(logflgraph)
 intersection_bf = betagraph.intersection(logflgraph)
 
-x, y = intersection(np.log10(range(fl.size)), alphaline, np.log10(range(fl.size)), logfl)
-plt.plot(np.log10(range(fl.size)), alphagraph)
-plt.plot(np.log10(range(fl.size)), logflgraph)
-plt.plot(x, y, '*k')
+# flag = False
+# i = 1
+# while (not flag) or (i != fl.size):
+#     i += 1
+#     aline = np.full(i, alpha * maxfl)
+#     agraph = LineString(np.column_stack((np.log10(range(i)), aline)))
+#    for j in range(i):
+#         flline[j] = np.log10(fl[j])
+#     intersection_afl = agraph.intersection(flline)
+#    print('Inter=', intersection_afl)
 
-#if intersection_af.geom_type == 'MultiPoint':
-#    plt.plot(*LineString(intersection_af).xy, 'o')
-#elif intersection_af.geom_type == 'Point':
-#    plt.plot(*intersection_af.xy, 'o')
+# idx = np.argwhere(np.diff(np.sign(alphaline - logfl))).flatten()
+# print('idx=', idx)
+# plt.plot(alphaline[idx], logfl[idx], 'ro')
+
+if intersection_af.geom_type == 'MultiPoint':
+    plt.plot(*LineString(intersection_af).xy, 'o')
+elif intersection_af.geom_type == 'Point':
+    plt.plot(*intersection_af.xy, 'o')
 
 # if intersection_bf.geom_type == 'MultiPoint':
 #    plt.plot(*LineString(intersection_bf).xy, 'o')
-#elif intersection_bf.geom_type == 'Point':
+# elif intersection_bf.geom_type == 'Point':
 #    plt.plot(*intersection_bf.xy, 'o')
-plt.axhline(y=maxfl, color='g') #  Line at maximum
-# plt.axhline(y=beta * maxfl, color='g') #  Line at beta % level
-# plt.axhline(y=alpha * maxfl, color='g') #  Line at alpha % level
+plt.axhline(y=maxfl, color='g')  # Line at maximum
+# plt.axhline(y=beta * maxfl, color='g')  #  Line at beta % level
+# plt.axhline(y=alpha * maxfl, color='g')  #  Line at alpha % level
 # plt.axvline(x=2, color='b')
 # plt.axvline(x=3.5, color='r')
 # plt.axhline(y=np.log10(fl[100]), color='b')
@@ -93,4 +102,5 @@ plt.axhline(y=maxfl, color='g') #  Line at maximum
 # x1, y1 = [0, 2], [np.log10(fl[1]), np.log10(fl[100])]
 # x2, y2 = [2, 3.5], [np.log10(fl[100]), np.log10(fl[int(10**3.5)])]
 # plt.plot(x1, y1, x2, y2)
+plt.axis('scaled')
 plt.show()
